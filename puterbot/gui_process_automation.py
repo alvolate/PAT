46d8119486b2327a3fcea3d71132e42a2cb5b977
alvolate <74493853+alvolate@users.noreply.github.com>
2023-04-13 15:28:07 -0400

    Update gui_process_automation.py

--- puterbot/gui_process_automation.py
+++ puterbot/gui_process_automation.py
@@ -1,16 +1,23 @@
-from segment_anything import SamPredictor, sam_model_registry,SamAutomaticMaskGenerator
+
 from transformers import GPTJForCausalLM, GPT2Tokenizer
 from paddleocr import PaddleOCR
 
 from models import InputEvent
 
-# Initialize Segment Anything model
-sam = sam_model_registry["<model_type>"](checkpoint="<path/to/checkpoint>")
-sam_predictor = SamPredictor(sam)
+import sys
+sys.path.append("..")
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
 
-#Initialize Segment Anything model to generate masks for an entire image:
+sam_checkpoint = "/Users/taeyangkim/puterbot/puterbot/sam_vit_h_4b8939.pth"
+model_type = "vit_h"
+
+device = "cuda"
+
+sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+
+#sam.to(device=device)
+
 mask_generator = SamAutomaticMaskGenerator(sam)
-
 
 # Initialize GPT-J model
 tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-j-6B")
